ff1bb9f78da56c7126c90f9e16c7a4011d694a36
M-Minkov <MinkoMinkov@outlook.co.nz>
2020-06-10 14:45:58 +1200

    Addde problem 44, edited problem 45

--- Problem 45/Naive Solution.py
+++ Problem 45/Naive Solution.py
@@ -3,20 +3,25 @@ def hexagonal(n: int) -> int:
     return (n*(2*n - 1))
 
 def is_pentagonal(n: int) -> bool:
-    temp = (1/6) + (((2*n/3) + (1/36))**0.5)
+    temp = 1+((1+24*n)**0.5)
 
-    if temp == int(temp):
+    if temp % 6 == 0:
         return True
-    else:
-        return False
-
-def is_triangle(n: int) -> bool:
-    temp = (-1 + ((1+8*n)**0.5))/2
     
-    if temp == int(temp):
-        return True
     else:
         return False
+
+def is_triangle(n: int):
+    temp = -1 + ((1+8*n)**0.5)
+    temp = temp/2
+
+    if temp == int(temp):
+        return temp
+    else:
+        return False
+
+def triangle(n: int) -> int:
+    return (n*(n+1)) << 1
 
 
 
@@ -27,7 +32,9 @@ n = 144
 while True:
     hex_n = hexagonal(n)
 
-    if is_pentagonal(hex_n) and is_triangle(hex_n):
+    if is_pentagonal(hex_n) and type(is_triangle(hex_n)) is int:
         print(n)
         break
+    n = n + 1
 
+print(is_triangle(n))
